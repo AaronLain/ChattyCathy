c562a489b436e7ea55778c5fbb028ce22c7bf4a3
WVM <aaron.j.lain@gmail.com>
2020-12-02 15:27:26 -0800

    signalr set up pt1

---
+++ ChattyCathy/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChattyCathy.Hubs.Clients;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChattyCathy.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+    }
+}
---
+++ ChattyCathy/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ChattyCathy.Models;
+
+namespace ChattyCathy.Hubs.Clients
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+    }
+}
