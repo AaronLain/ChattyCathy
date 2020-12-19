709e9c6f48b473beff7db1b127e2485c3c87affe
Aaron Lain <aaron.j.lain@gmail.com>
2020-12-19 12:13:03 -0600

    SECRETS WORK

--- chattycathy.ui/src/Components/Helpers/Cathy/Cathy.js
+++ chattycathy.ui/src/Components/Helpers/Cathy/Cathy.js
@@ -1,9 +1,16 @@
 import messageData from '../Data/messageData';
 import moment from 'moment';
+import { ConsoleLogger } from '@microsoft/signalr/dist/esm/Utils';
 
 const responses = messageData.getResponses()
 
 const greetings = ['hello', 'hi', 'hey', 'greetings', 'salutations']
+
+const secretFetch = () => messageData.getSecretById(2).then(result => {
+    return result
+})
+
+
 
 const secretTriggers = ['tell me a secret', 'secret', 'secrets']
 
@@ -20,13 +27,16 @@ const cathySummoner = (user, message) => {
 // if not, returns random response
 const greetingCheck = (user, message) => {
     const rand = Math.floor(Math.random() * (responses.length));
-    if (greetings.some(g => message.includes(g))) {
-        return `Sup ${user}`;
-    } else if (secretTriggers.some(s => message.includes(s))) {
-        return 'Secrets secrets are no fun'
-    } else {
-        return `${responses[rand]}`
-    }
+    return secretFetch().then((secret) => {
+        console.log(typeof secret, 'secret')
+        if (greetings.some(g => message.includes(g))) {
+            return `Sup ${user}`;
+        } else if (secretTriggers.some(s => message.includes(s))) {
+            return secret;
+        } else {
+            return `${responses[rand]}`
+        }
+    })
 }
 
 const sentimentAnalyzer = async (message) => {
@@ -34,19 +44,21 @@ const sentimentAnalyzer = async (message) => {
 }
 
 const cathyMessage = async (user, message) => {
-    const chatMessage = {
-        userName: 'Cathy',
-        content: greetingCheck(user, message),
-        userId: 666,
-        sentiment: 0,
-        date: moment(),
-    }
-    try {
-        await messageData.postMessage(chatMessage);
-    }
-    catch(err) {
-        console.log(err);
-    }
+    greetingCheck(user, message).then((response) => {
+        const chatMessage = {
+            userName: 'Cathy',
+            content: response,
+            userId: 666,
+            sentiment: 0,
+            date: moment(),
+        }
+        try {
+            return messageData.postMessage(chatMessage);
+        }
+        catch(err) {
+            console.log(err);
+        }
+    });
 }
 
 export default {cathySummoner};
