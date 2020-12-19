57a8ddd78cd6adf5d67c81a3e3d1389c613445c8
Aaron Lain <aaron.j.lain@gmail.com>
2020-12-19 10:21:38 -0600

    lol

--- chattycathy.ui/src/Components/Helpers/Cathy/Cathy.js
+++ chattycathy.ui/src/Components/Helpers/Cathy/Cathy.js
@@ -3,12 +3,17 @@ import moment from 'moment';
 
 const responses = messageData.getResponses()
 
+const secrets = messageData.getSecrets()
+
 const greetings = ['hello', 'hi', 'hey', 'greetings', 'salutations']
+
+const secretTriggers = ['secret', 'tell me a secret', 'tell secret', 'secrets']
 
 const cathySummoner = (user, message) => {
     if(message.includes('@cathy')) {
         setTimeout(() => {
             cathyMessage(user, message)
+            console.log(secrets)
         }, 2600);
         
     }
@@ -16,15 +21,20 @@ const cathySummoner = (user, message) => {
 
 // checks if the message includes any greeting triggers,
 // if not, returns random response
-const greetingCheck = (user, message) => {
+const greetingCheck = async (user, message) => {
     const rand = Math.floor(Math.random() * (responses.length));
     if (greetings.some(g => message.includes(g))) {
         return `Sup ${user}`;
+    } else if (secretTriggers.some(s => message.includes(s))) {
+        return 'please stand by'
     } else {
         return `${responses[rand]}`
     }
 }
 
+const sentimentAnalyzer = async (message) => {
+    ///TBD 
+}
 
 const cathyMessage = async (user, message) => {
     const chatMessage = {
